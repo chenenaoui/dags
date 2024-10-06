7cfc73a479fdaeb95dacf53515acbe99f7e6bc39
ghofrane chenenaoui <36867301+chenenaoui@users.noreply.github.com>
2024-10-06 18:48:50 +0100

    Add files via upload

---
+++ demo2.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+# Define the DAG and its arguments
+dag = DAG(
+    'simple_dag',
+    description='A simple example DAG',
+    schedule_interval='@daily',
+    start_date=days_ago(1),
+)
+
+# Define tasks
+start = DummyOperator(task_id='start', dag=dag)
+end = DummyOperator(task_id='end', dag=dag)
+
+# Set the task sequence
+start >> end
